882d4e61f62333b5010817af13fcc3cc2cde7a4a
iemxia <xmw2003@gmail.com>
2024-03-05 19:13:43 -0800

    player movement working

--- whiskers-and-whispers/Assets/Scripts/PlayerMovement.cs
+++ whiskers-and-whispers/Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,16 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
